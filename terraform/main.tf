5ae80e28851e019bec94af89a188bf5f6a6c5839
logesh981 <logesh.venkat@gmail.com>
2025-01-05 00:51:58 +0000

    fixing main.tf by adding data sources for kubectl

--- terraform/main.tf
+++ terraform/main.tf
@@ -106,11 +106,13 @@ module "eks" {
 }
 
 data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_name
+  depends_on = [module.eks]
+  name       = module.eks.cluster_name
 }
 
 data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_name
+  depends_on = [module.eks]
+  name       = module.eks.cluster_name
 }
 
 resource "kubectl_manifest" "example" {
