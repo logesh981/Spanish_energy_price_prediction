14411bef6219d843b7575a04e02b16465b14faff
logesh981 <logesh.venkat@gmail.com>
2025-01-06 12:53:44 +0000

    making changes to  provider.tf to use the correct cluster endpoint

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -14,7 +14,7 @@ provider "aws" {
   region = "us-east-1"
 }
 provider "kubectl" {
-  host                   = module.eks.cluster_endpoint
+  host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
   token                  = data.aws_eks_cluster_auth.cluster.token
 }
