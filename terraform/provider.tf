9826ac57706d417bea1eb20403abdbe4dfa7f6a3
logesh981 <logesh.venkat@gmail.com>
2025-01-09 10:57:16 +0000

    Update CD workflow

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -17,4 +17,5 @@ provider "kubectl" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
   token                  = data.aws_eks_cluster_auth.cluster.token
+  load_config_file       = false
 }
