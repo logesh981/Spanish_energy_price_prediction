2272ba9905c3958427af1233b2a871067e9afd61
Logesh_98 <logesh.venkat@gmail.com>
2025-03-02 21:49:22 +0000

    modify the app so that model loads only once and some better logging

--- predict.py
+++ predict.py
@@ -1,9 +1,8 @@
 import os
-
+import logging
 
 from flask import Flask, request, jsonify
 import model
-
 
 # with open('xgmodel.bin','rb') as f_in:
 #     model = pickle.load(f_in)
@@ -14,21 +13,30 @@ import model
 
 app = Flask('energy-price-prediction')
 
+RUN_ID = os.getenv("MLFLOW_RUN_ID")
+if RUN_ID is None:
+    raise ValueError("MLFLOW_RUN_ID is not set in the environment variables")
+
+try:
+    modelt = model.load_model(RUN_ID)
+    model_service = model.ModelService(modelt)
+except Exception as e:
+    logging.error("Failed to load the model: %s", str(e))
+    raise e
+
 
 @app.route('/predict', methods=['POST'])
 def predict_endpoint():
     energy_val = request.get_json()
-    run_id = os.getenv("MLFLOW_RUN_ID")
-    modelt = model.load_model(run_id)
-    model_service = model.ModelService(modelt)
+
+    # Validate input
+    if not isinstance(energy_val, dict):
+        return jsonify({"error": "Invalid input format. Expected JSON."}), 400
+
     modified_features = model_service.prepare_features(energy_val)
     prediction = model_service.predict(modified_features)
-    # print("")
-
-    # return jsonify(result)
 
     result = {"pred": float(prediction[0])}
-
     return jsonify(result)
 
 
